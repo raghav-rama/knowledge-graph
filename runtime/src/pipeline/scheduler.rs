71d3aaefe7f2529cca75839bc9837daa540559aa
raghav-rama <hackerer528@gmail.com>
2025-11-12 17:24:23 +0530

    add other relevant structs related to scheduler

--- runtime/src/pipeline/scheduler.rs
+++ runtime/src/pipeline/scheduler.rs
@@ -3,3 +3,41 @@ struct Scheduler {
     dispatcher: Dispatch,
     worker: Worker,
 }
+
+struct Queue {
+    jobs: Vec<Job>,
+    capacity: u32,
+}
+
+enum JobStatus {
+    Processing,
+    Done,
+    Failed,
+    PartiallyFailed,
+}
+
+struct Job {
+    job_id: String,
+    doc_id: String,
+    max_retries: u8,
+    current_retry: u8,
+    job_status: JobStatus,
+    chunks: Vec<ChunkState>,
+    create_at: DateTime<Utc>,
+    next_run_at: Instant,
+    last_error: Option<String>,
+}
+
+struct ChunkState {
+    chunk_id: String,
+    chunk_status: ChunkStatus,
+    error: Option<String>,
+    output: Option<String>,
+    max_retries: u8,
+    current_retry: u8,
+}
+
+enum ChunkStatus {
+    Success,
+    Failed,
+}
